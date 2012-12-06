a6c6d5310319b800f9807d116a06a72e73031765
Maxim Colls <collsmaxim@gmail.com>
2012-12-06 11:30:33 +0100

    Initial commit, new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZohMyRecords::Application.config.secret_token = '9d0f39711c55fe2a10f6fa5a4141a9198c1b0d292f18be1d6c9747277227f96fda7cc7bd7a742b81338f559f57ace5ee38ea8c5fef09e507f97480af4e3acaf6'
