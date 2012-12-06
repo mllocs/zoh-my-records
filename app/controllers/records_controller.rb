21409e66f264ea31dc794ec398f69e032863dc00
Maxim Colls <collsmaxim@gmail.com>
2012-12-06 13:09:11 +0100

    Add functional tests and implements records controller to respond to json

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,2 +1,26 @@
 class RecordsController < ApplicationController
+
+  respond_to :json
+
+  def index
+    @records = Record.all
+    respond_with(@records)
+  end
+
+  def create
+    @record = Record.new(params[:record])
+    respond_with @record
+  end
+
+  def update
+    @record = Record.find(params[:id])
+    @record.update_attributes(params[:record])
+    respond_with @record
+  end
+
+  def destroy
+    @record = Record.find(params[:id])
+    @record.destroy
+    respond_with @record
+  end
 end
--- test/functional/records_controller_test.rb
+++ test/functional/records_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class RecordsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should respond index" do
+    get :index, format: :json
+    assert_response :success
+    assert_not_nil assigns(:records)
+  end
+
+  test "should respond create" do
+    post :create, format: :json, record: { title: "The Misterious Production Of Eggs", artist: "Andrew Bird" }
+    assert_response :success
+    assert_not_nil assigns(:record)
+  end
+
+  test "should respond update" do
+    put :update, format: :json, id: records(:one).id, record: { title: "The Heart Of Saturday Night"}
+    assert_response :success
+    assert_equal "The Heart Of Saturday Night", Record.find(records(:one).id).title
+    assert_not_nil assigns(:record)
+  end
+
+  test "should respond delete" do
+    assert_difference('Record.count', -1) do
+      delete :destroy, format: :json, id: records(:two).id
+    end
+    assert_response :success
+  end
+
 end
