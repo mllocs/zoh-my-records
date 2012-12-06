eb713583fa0bb73ea68c4c8b9d79855e4b5272d6
Maxim Colls <collsmaxim@gmail.com>
2012-12-06 20:10:11 +0100

    Add records to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Record.create!(
+  title: "Low Edges", 
+  artist: "Richard Hawley", 
+  cover_url: "http://cdn.krrb.com/post_images/photos/000/000/361/Richard_Hawley_Lowedges1_large.jpg", 
+  spotify_uri: "spotify:album:6O2x7QN3BqUeXBaH8Fot49"
+)
+
+Record.create!(
+  title: "The Heart Of Sunday Night", 
+  artist: "Tom Waits", 
+  cover_url: "http://ijustreadaboutthat.files.wordpress.com/2011/09/75112.jpg", 
+  spotify_uri: "spotify:track:23X3UiWmkM09Ug2heM6z38"
+)
+
+Record.create!(
+  title: "OH (Ohio)", 
+  artist: "Lambchop", 
+  cover_url: "http://expressmilwaukee.com/imgs/hed/art5489widea.jpg", 
+  spotify_uri: "spotify:album:4y9ebrmAx1TMyUlyCVJJL6"
+)
+
+# Record.create!(
+#   title: "", 
+#   artist: "", 
+#   cover_url: "", 
+#   spotify_uri: ""
+# )
